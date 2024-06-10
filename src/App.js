8abd1dcac265c23f632d8a2cb21cddcbc62bf6a9
deysiSc76 <deysi.santacruz76@gmail.com>
2024-06-10 10:36:42 -0400

    Ejercicio de Enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Routes,
+  Route,
+  useParams
+} from 'react-router-dom';
 
-function App() {
+// Componente para la página de inicio
+const Home = () => <h1>Bienvenida</h1>;
+
+// Componente que muestra un número o indica que no es un número
+const NumberOrWord = () => {
+  const { value } = useParams();
+  return isNaN(+value) ? <h1>this is word:  {value}</h1> : <h1>this is number: {value}</h1>;
+};
+
+// Componente que muestra una palabra con colores de texto y fondo personalizados
+const ColoredWord = () => {
+  const { word, textColor, bgColor } = useParams();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 style={{ color: textColor, backgroundColor: bgColor }}>this is word: {word}</h1>
   );
-}
+};
+
+// Aplicación principal con enrutamiento configurado
+const App = () => {
+  return (
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/:value" element={<NumberOrWord />} />
+        <Route path="/:word/:textColor/:bgColor" element={<ColoredWord />} />
+      </Routes>
+  );
+};
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
+// Crea una raíz de React y monta tu aplicación en el elemento con id 'root'.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
